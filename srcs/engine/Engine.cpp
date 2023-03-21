e514ed53c5331a6f1b8721551139d88fc0e77def
Álvaro Rodríguez <alvaromarianorodriguezbenitez@gmail.com>
2023-03-21 10:07:00 +0100

    [ADD] Example mesh

--- srcs/engine/Engine.cpp
+++ srcs/engine/Engine.cpp
@@ -1,9 +1,47 @@
 #include "Engine.hpp"
 
-const int N_POINTS = 9 * 9 * 9;
-std::vector<Vector3> cubePoints;
-std::vector<Vector2> projectedPoints(N_POINTS);
 const float fov_factor = 640;
+
+typedef struct {
+    Vector2 points[3];
+} triangle_t;
+
+triangle_t trianglesToRender[12];
+
+Vector3 mesh_vertices[8] = {
+    Vector3(-1, -1, -1),
+    Vector3(-1, 1, -1),
+    Vector3(1, 1, -1),
+    Vector3(1, -1, -1),
+    Vector3(1, 1, 1),
+    Vector3(1, -1, 1),
+    Vector3(-1, 1, 1),
+    Vector3(-1, -1, 1)
+};
+
+typedef struct {
+    int a, b, c;
+} face_t;
+
+face_t mesh_faces[12] = {
+    {1, 2, 3},
+    {1, 3, 4},
+
+    {4, 3, 5},
+    {4, 5, 6},
+
+    {6, 5, 7},
+    {6, 7, 8},
+
+    {8, 7, 2},
+    {8, 2, 1},
+
+    {2, 7, 5},
+    {2, 5, 3},
+
+    {6, 8, 1},
+    {6, 1, 4},
+};
 
 Engine::Engine() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -32,21 +70,12 @@ Engine::Engine() {
     ));
 
     this->_cameraPosition = Vector3(0, 0, -5.0f);
-
-    for (float x = -1; x <= 1; x += 0.25) {
-        for (float y = -1; y <= 1; y += 0.25) {
-            for (float z = -1; z <= 1; z += 0.25) {
-                cubePoints.push_back(Vector3(x, y, z));
-            }
-        }
-    }
-
     this->_previousFrametime = 0;
     this->_running = true;
 }
 
 Engine::Engine(const Engine& other) {
-
+    //TODO: Copy constructor
 }
 
 Engine::~Engine() {
@@ -56,7 +85,8 @@ Engine::~Engine() {
     SDL_Quit();
 }
 
-Engine &Engine::operator = (const Engine& other) {
+Engine &Engine::operator=(const Engine& other) {
+    //TODO: Copy assignment
     return *this;
 }
 
@@ -79,10 +109,6 @@ void Engine::processInput() {
         case SDL_KEYDOWN:
             if (event.key.keysym.sym == SDLK_ESCAPE)
                 this->_running = false;
-            else if (event.key.keysym.sym == SDLK_s)
-                this->_cameraPosition.z -= 0.25;
-            else if (event.key.keysym.sym == SDLK_w)
-                this->_cameraPosition.z += 0.25;
             break;
     }
 }
@@ -95,24 +121,44 @@ void    Engine::update() {
     
     this->_previousFrametime = SDL_GetTicks();
 
-    for (int i = 0; i < N_POINTS; i++) {
-        Vector3 point = cubePoints[i];
-        point.z -= this->_cameraPosition.z;
+    for (int i = 0; i < 12; i++) {
+        face_t mesh_face = mesh_faces[i];
 
-        cubePoints[i].rotateX(0.01);
-        cubePoints[i].rotateY(0.01);
-        cubePoints[i].rotateZ(0.01);
+        Vector3 face_vertices[3];
+        face_vertices[0] = mesh_vertices[mesh_face.a - 1];
+        face_vertices[1] = mesh_vertices[mesh_face.b - 1];
+        face_vertices[2] = mesh_vertices[mesh_face.c - 1];
 
-        Vector2 projectedPoint = this->project(point);
-        projectedPoints[i] = projectedPoint;
+        triangle_t projected_triangle;
+        for (int j = 0; j < 3; j++) {
+            Vector3 transformed = face_vertices[j];
+            
+            transformed.z -= _cameraPosition.z;
+            Vector2 projected = this->project(transformed);
+
+            projected.x += WIDTH / 2;
+            projected.y += HEIGHT / 2;
+
+            projected_triangle.points[j] = projected;
+        }
+
+        trianglesToRender[i] = projected_triangle;
     }
 }
 
 void    Engine::render() {
     this->_colorBuffer->drawGrid();
 
-    for (int i = 0; i < N_POINTS; i++) {
-        this->_colorBuffer->drawRect(projectedPoints[i].x + WIDTH / 2, projectedPoints[i].y + HEIGHT / 2, 4, 4, 0xFFFF0000);
+    for (int i = 0; i < 12; i++) {
+        triangle_t triangle = trianglesToRender[i];
+
+        //std::cout << "(" << triangle.points[0].x << ", " << triangle.points[0].y << ")" << std::endl;
+        //std::cout << "(" << triangle.points[1].x << ", " << triangle.points[1].y << ")" << std::endl;
+        //std::cout << "(" << triangle.points[2].x << ", " << triangle.points[2].y << ")" << std::endl;
+
+        this->_colorBuffer->drawRect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
+        this->_colorBuffer->drawRect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
+        this->_colorBuffer->drawRect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
     }
 
     this->_colorBuffer->render(this->_renderer);
